db7dad9e905a6e9f44af15f17de94033ec34a0f2
omarabdiwali <omarabdiwali17@gmail.com>
2023-06-23 22:59:57 -0600

    load page

--- pages/api/public/access.js
+++ pages/api/public/access.js
@@ -5,13 +5,18 @@ import Public from "@/models/Public";
 
 export default async function handler(req, res) {
   const session = await getServerSession(req, res, authOptions);
+  let email;
 
-  if (!req.body || !session) {
+  if (!req.body) {
     res.redirect("/");
     return;
   }
 
-  const profile = session.user;
+  if (session) {
+    const profile = session.user;
+    email = profile.email;
+  }
+
 
   const { code } = JSON.parse(req.body);
   let query = { id: code };
@@ -20,7 +25,7 @@ export default async function handler(req, res) {
   let folder = await Public.findOne(query);
 
   if (folder) {
-    if (folder.public || folder.email === profile.email) {
+    if (folder.public || folder.email === email) {
       res.status(200).send({ folder: folder, answer: "Folder is public." });
     } else {
       res.status(200).send({ answer: "Folder is private." });
