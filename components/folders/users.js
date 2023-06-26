111541ea81cb8bfa6a987c6c80ffdecfbef5281a
omarabdiwali <omarabdiwali17@gmail.com>
2023-06-26 00:18:18 -0600

    changed color

--- components/folders/users.js
+++ components/folders/users.js
@@ -2,7 +2,7 @@ import { BsXLg } from "react-icons/bs";
 
 export default function Users({ email, remove }) {
   return (
-    <div className="flex text-lg text-slate-700 mx-5 rounded-xl mt-3 space-x-5 bg-slate-400 h-12 max-h-12">
+    <div className="flex text-lg text-black mx-5 rounded-xl mt-3 space-x-5 bg-slate-400 h-12 max-h-12">
       <button onClick={() => remove(email)} className="mx-3 m-auto"><BsXLg /></button>
       <div className="flex-1 m-auto">{email}</div>
     </div>
