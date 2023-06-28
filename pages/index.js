ec1be65b019ff0820c57b5f81b644ebefa0c2655
omarabdiwali <omarabdiwali17@gmail.com>
2023-06-28 11:10:15 -0600

    change 'if' statement

--- pages/index.js
+++ pages/index.js
@@ -58,7 +58,7 @@ export default function Home() {
     window.location.href = `/search/${searchValue}`;
   }
 
-  if (!session && status === 'unauthenticated') {
+  if (status === 'unauthenticated') {
     return (
       <>
         <Head>
