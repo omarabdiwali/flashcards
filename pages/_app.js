399b796e99eeeea8bf39ccb7affffc1fb3153514
Omar Abdiwali <omarabdiwali17@gmail.com>
2023-08-27 05:24:08 -0600

    seo

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,15 @@
 import '@/styles/globals.css'
 import { SessionProvider } from "next-auth/react"
+import Head from 'next/head'
 import { SnackbarProvider } from 'notistack'
 
 export default function App({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <SnackbarProvider preventDuplicate>
       <SessionProvider session={session}>
+        <Head>
+          <meta name="google-site-verification" content="AFBrxKgnj1l1SuZP7euHyl2zmEnTh5kGcTZgsZZ7bMc" />
+        </Head>
         <Component {...pageProps} />
       </SessionProvider>
     </SnackbarProvider>
